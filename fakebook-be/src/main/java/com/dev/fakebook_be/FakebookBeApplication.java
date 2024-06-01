c13f1f11c4ea283ac58569a9a954a3cddaea03e9
trongVinhh <21020800@vnu.edu.vn>
2024-06-02 00:35:56 +0700

    add base backend spring boot

---
+++ fakebook-be/src/main/java/com/dev/fakebook_be/FakebookBeApplication.java
@@ -0,0 +1,13 @@
+package com.dev.fakebook_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakebookBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakebookBeApplication.class, args);
+	}
+
+}
---
+++ fakebook-be/src/test/java/com/dev/fakebook_be/FakebookBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.fakebook_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakebookBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
